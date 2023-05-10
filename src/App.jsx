35339f589237961c25452a7afe7d4bd56359ee25
Nazmul Hussain <nazmul.hussain.utchash@gmail.com>
2023-05-10 18:24:35 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { useLoaderData } from "react-router-dom"
 import CoffeeCard from "./Components/CoffeeCard"
+import { useState } from "react";
 
 
 function App() {
 
-  const coffees = useLoaderData()
-  console.log(coffees);
+  const loadedCoffees = useLoaderData()
+  const [coffees, setCoffees] = useState(loadedCoffees)
+
+
   return (
     <div className="container mx-auto">
       <h1 className="text-4xl font-bold text-center my-8">Hoot Hoot Coffee: {coffees.length}</h1>
       <div className="grid grid-cols-2 gap-4">
         {
-          coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee} ></CoffeeCard>)
+          coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee} coffees={coffees} setCoffees={setCoffees}></CoffeeCard>)
         }
       </div>
     </div>
